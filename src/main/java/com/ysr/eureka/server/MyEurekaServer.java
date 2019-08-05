aa475c6d4df329a3a1184bf0b0b5a2eaa8408340
yangshizhou <933831728@qq.com>
2019-08-05 22:39:32 +0800

    eureka

---
+++ src/main/java/com/ysr/eureka/server/MyEurekaServer.java
@@ -0,0 +1,13 @@
+package com.ysr.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MyEurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurekaServer.class);
+    }
+}
